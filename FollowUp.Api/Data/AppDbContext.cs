980bf6e9ac5df55840c14cceca8d22c5832c844a
Aishi Baral <aishi369baral@gmail.com>
2026-02-16 09:58:39 +0530

    Day 1: initial backend setup with SQLite and EF Core

---
+++ FollowUp.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FollowUp.Api.Models;
+
+namespace FollowUp.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+}
